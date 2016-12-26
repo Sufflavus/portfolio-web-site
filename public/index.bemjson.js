99975cc70b5d13bf21e68cdf3a3e6e6811bd2cd7
Tania <tatashat@gmail.com>
2016-12-26 21:49:18 +0000

    * styles

--- public/index.bemjson.js
+++ public/index.bemjson.js
@@ -38,7 +38,7 @@
             ]
         },
         {
-            block : 'section-home',
+            block : 'section',
             content : [
                 {
                     elem : 'about',
